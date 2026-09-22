c7d296119e28207fdeac7d9af0d0107e56fa1bbf
Gaurav Prasanna <55285968+circuit-geek@users.noreply.github.com>
2026-09-22 17:11:21 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+
+        left = 0
+        right = 1
+
+        while right < len(nums):
+            if nums[left] == nums[right]:
+                return True
+
+            left += 1
+            right += 1
+        
+        return False
+            
+            
+        
